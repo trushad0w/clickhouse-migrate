8f090cbdef7657ee218a1a4d437ddb48349b70c7
Pavel Trunov <pavel.trounov@smart-steel-technologies.com>
2022-04-05 17:35:34 +0200

    Black formatting

--- clickhouse_migrate/conf/settings.py
+++ clickhouse_migrate/conf/settings.py
@@ -20,14 +20,25 @@ class Settings(metaclass=Singleton):
     CONFIG_PATH = path.join("./clickhouse_migrate.ini")
     DEFAULT_MIGRATIONS_DIR = path.join("./migrations")
 
-    databases = json.loads(CLICKHOUSE_MIGRATE_DATABASES) if CLICKHOUSE_MIGRATE_DATABASES else None
+    databases = (
+        json.loads(CLICKHOUSE_MIGRATE_DATABASES)
+        if CLICKHOUSE_MIGRATE_DATABASES
+        else None
+    )
     migration_dir = CLICKHOUSE_MIGRATE_DIRECTORY
 
-    def init_config(self, config_path: Optional[str] = CONFIG_PATH, databases: Optional[str] = None, migration_dir: Optional[str] = DEFAULT_MIGRATIONS_DIR):
+    def init_config(
+        self,
+        config_path: Optional[str] = CONFIG_PATH,
+        databases: Optional[str] = None,
+        migration_dir: Optional[str] = DEFAULT_MIGRATIONS_DIR,
+    ):
         if self.databases is None:
 
             if not databases:
-                databases, migration_dir = self.__get_params_from_config_file(config_path=config_path)
+                databases, migration_dir = self.__get_params_from_config_file(
+                    config_path=config_path
+                )
 
             self.databases = databases
             self.migration_dir = migration_dir
@@ -40,17 +51,25 @@ class Settings(metaclass=Singleton):
             config = configparser.ConfigParser()
             config.read(config_path)
 
-            return json.loads(config.get("databases", "connection_strings")), json.loads(config.get("migration_path", "directory"))
+            return json.loads(
+                config.get("databases", "connection_strings")
+            ), json.loads(config.get("migration_path", "directory"))
         except KeyError:
-            raise ConfigError("Variables: connection_strings, directory must be provided in the *.ini config file")
+            raise ConfigError(
+                "Variables: connection_strings, directory must be provided in the *.ini config file"
+            )
 
     def __post_init_checks(self):
         if self.databases is None:
-            raise ConfigError("Unable to configure migration tool. None of the configuration options were provided")
+            raise ConfigError(
+                "Unable to configure migration tool. None of the configuration options were provided"
+            )
         if not path.isdir(self.migration_dir):
-            raise ConfigError("Unable to configure migration tool. Provided migration dir does not exist")
+            raise ConfigError(
+                "Unable to configure migration tool. Provided migration dir does not exist"
+            )
 
 
 if __name__ == "__main__":
     settings = Settings()
-    settings.init_config(config_path='./tests/clickhouse_migrate_test.ini')
+    settings.init_config(config_path="./tests/clickhouse_migrate_test.ini")
